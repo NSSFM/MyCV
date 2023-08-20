036f7f6d3a8e3844aa801ee139cf66a57049ca17
Jan <u.sem@icloud.com>
2023-08-20 23:38:50 +0200

    Updated styles and added theme toggle button

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-const header = document.querySelector('header');
+const themeToggle = document.getElementById("theme-toggle");
+const body = document.body;
 
-header.addEventListener('click', () => {
-    header.style.color = getRandomColor();
+themeToggle.addEventListener("click", () => {
+    body.classList.toggle("dark-mode");
+    body.classList.toggle("light-mode"); 
 });
 
-function getRandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    body.classList.add("dark-mode");
+} else {
+    body.classList.add("light-mode"); 
 }
